0265cc9adc701ae309c1010753f910ac1576bf38
aeconick <114342845+aeconick@users.noreply.github.com>
2023-03-10 18:41:10 +0200

    Added global css

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,22 @@
 import { Footer } from "./components/Footer";
 import { Header } from "./components/Header";
+import { Search } from "./components/Search";
+
+import './App.css';
+
 
 function App() {
   return (
     <>
-    <Header />
-    <Footer />
+      <Header />
+
+      <main class="main">
+
+        <Search />
+
+      </main>
+
+      <Footer />
     </>
   );
 }
