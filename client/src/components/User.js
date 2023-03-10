94ed957c3eee2e9a7b7da16ffc254a2003ad58a0
aeconick <114342845+aeconick@users.noreply.github.com>
2023-03-10 22:09:07 +0200

    Added user details functionality

--- client/src/components/User.js
+++ client/src/components/User.js
@@ -1,3 +1,5 @@
+import { formatDate } from '../utils/dateUtils';
+
 export const User = ({
   firstName,
   lastName,
@@ -6,7 +8,8 @@ export const User = ({
   createdAt,
   phoneNumber,
   updatedAt,
-  _id
+  _id,
+  onInfoClick,
 }) => {
   return (
     <tr>
@@ -17,7 +20,7 @@ export const User = ({
       <td>{lastName}</td>
       <td>{email}</td>
       <td>{phoneNumber}</td>
-      <td>{createdAt}</td>
+      <td>{formatDate(createdAt)}</td>
 
       <td className="actions">
         <button className="btn edit-btn" title="Edit">
@@ -37,7 +40,7 @@ export const User = ({
             </path>
           </svg>
         </button>
-        <button className="btn info-btn" title="Info">
+        <button className="btn info-btn" title="Info" onClick={() => onInfoClick(_id)}>
           <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="info"
             className="svg-inline--fa fa-info" role="img" xmlns="http://www.w3.org/2000/svg"
             viewBox="-150 0 512 612">
