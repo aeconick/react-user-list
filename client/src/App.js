856c7d1b623e68f8b0316a58770d460c39c9848b
aeconick <114342845+aeconick@users.noreply.github.com>
2023-03-10 20:31:07 +0200

    Render users from db

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,39 @@
+import { useEffect, useState } from 'react';
+
+import './App.css';
+import * as userService from './services/UserService';
+
 import { Footer } from "./components/Footer";
 import { Header } from "./components/Header";
 import { Search } from "./components/Search";
-
-import './App.css';
+import { UserList } from "./components/UserList";
 
 
 function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    userService.getAll()
+      .then(users => {
+        setUsers(users);
+      })
+      .catch(err => {
+        console.log('Error' + err);
+      })
+  }, [])
+
   return (
     <>
       <Header />
 
-      <main class="main">
+      <main className="main">
+        <section className="card users-container">
 
-        <Search />
+          <Search />
 
+          <UserList users={users} />
+
+        </section>
       </main>
 
       <Footer />
